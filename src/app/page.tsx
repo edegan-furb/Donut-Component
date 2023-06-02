d0ea172061dc9dbc82571ba7b19e4c50888ba993
Eduardo Degan <edegan@furb.br>
2023-06-01 21:55:36 -0300

    donut

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+import Donut from '@/components/donut'
+
+export default function Home() {
+  return (
+    <div>
+      <div className="h-screen bg-cyan-950 text-yellow-300">
+        <h1 className="absolute left-48 top-52 text-5xl font-bold">
+          Donut Component ---{'>'}
+        </h1>
+        <div className="absolute -top-4 right-64">
+          <Donut />
+        </div>
+      </div>
+      <div className="fixed bottom-4 right-10 font-semibold text-yellow-300">
+        <a href="https://www.a1k0n.net/2011/07/20/donut-math.html">
+          Source --{'>'} a1k0n
+        </a>
+      </div>
+      <div className="fixed bottom-4 left-10 font-semibold text-yellow-300">
+        <a href="https://github.com/edegan-furb">Made by --{'>'} Edegan</a>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/donut.tsx
@@ -0,0 +1,87 @@
+'use client'
+
+import React, { useEffect, useRef, useCallback } from 'react'
+
+const DonutAnimation: React.FC = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const tmr2 = useRef<number | undefined>(undefined)
+  const A = useRef(1)
+  const B = useRef(1)
+
+  const R1 = 2 / 2.5
+  const R2 = 4 / 2.5
+  const K1 = 300 / 2.5
+  const K2 = 10 / 2.5
+
+  const canvasframe = useCallback(() => {
+    const canvas = canvasRef.current
+    if (!canvas) return
+
+    const ctx = canvas.getContext('2d')
+    if (!ctx) return
+
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+
+    A.current += 0.07
+    B.current += 0.03
+
+    const cA = Math.cos(A.current)
+    const sA = Math.sin(A.current)
+    const cB = Math.cos(B.current)
+    const sB = Math.sin(B.current)
+
+    for (let j = 0; j < 6.28; j += 0.3) {
+      const ct = Math.cos(j)
+      const st = Math.sin(j)
+
+      for (let i = 0; i < 6.28; i += 0.1) {
+        const sp = Math.sin(i)
+        const cp = Math.cos(i)
+
+        const ox = R2 + R1 * ct
+        const oy = R1 * st
+
+        const x = ox * (cB * cp + sA * sB * sp) - oy * cA * sB
+        const y = ox * (sB * cp - sA * cB * sp) + oy * cA * cB
+        const ooz = 1 / (K2 + cA * ox * sp + sA * oy)
+        const xp = 320 + K1 * ooz * x
+        const yp = 240 - K1 * ooz * y
+
+        const L =
+          0.7 *
+          (cp * ct * sB -
+            cA * ct * sp -
+            sA * st +
+            cB * (cA * st - ct * sA * sp))
+
+        if (L > 0) {
+          ctx.fillStyle = `rgba(255,255,255,${L})`
+          ctx.fillRect(xp, yp, 2.5, 2.5)
+        }
+      }
+    }
+  }, [K1, K2, R1, R2])
+
+  useEffect(() => {
+    const startAnimation = () => {
+      if (!tmr2.current) {
+        tmr2.current = window.setInterval(canvasframe, 50)
+      }
+    }
+
+    startAnimation()
+
+    return () => {
+      if (tmr2.current) {
+        window.clearInterval(tmr2.current)
+        tmr2.current = undefined
+      }
+    }
+  }, [canvasframe])
+
+  return (
+    <canvas ref={canvasRef} id="canvasdonut" width={640} height={480}></canvas>
+  )
+}
+
+export default DonutAnimation
